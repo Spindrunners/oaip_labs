34a38b02583fc4df503eca6e1bb0449e3529d875
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:14:38 +0300

    Add files via upload

---
+++ wwp6.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a, b, c, d, e, f;
+	printf("enter a=");
+	scanf_s("%d", &a);
+	printf("enter b=");
+	scanf_s("%d", &b);
+	printf("enter c=");
+	scanf_s("%d", &c);
+	d = a;
+	e = b;
+	f = c;
+	a = f;
+	c = e;
+	b = d;
+	printf("a %d\n", a);
+	printf("b %d\n", b);
+	printf("c %d\n", c);
+	return 0;
+}
