ec01a63391d8abd8b9427542eca3acfc2c27b188
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 15:58:56 +0300

    Add files via upload

---
+++ wwp4.3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int a, b;
+	float c;
+	printf("a =");
+	scanf_s("%d", &a);
+	printf("b =");
+	scanf_s("%d", &b);
+	c = (a + b) / 2;
+	printf("c =%f", c);
+}
