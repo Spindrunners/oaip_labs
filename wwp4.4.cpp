11c71cea5db20d0fdc877542bd3710e7afa2a9a0
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:00:46 +0300

    Add files via upload

---
+++ wwp4.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float a, b, proizv, akvadrat, summa, bkvadrat, chastnoe, raznost;
+	a != 0;
+	b != 0;
+	scanf_s("%f", &a);
+	scanf_s("%f", &b);
+	summa = a + b;
+	raznost = a - b;
+	proizv = a * b;
+	akvadrat = a * a;
+	bkvadrat = b * b;
+	chastnoe = akvadrat / bkvadrat;
+	printf("a + b %f\n", summa);
+	printf("a - b %f\n", raznost);
+	printf("a * b %f\n", proizv);
+	printf("akvadrat / bkvadrat %f", chastnoe);
+	return 0;
+}
