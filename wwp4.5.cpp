eb1c6dc249c8f819e025f403df117e706b3802e2
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:03:43 +0300

    Add files via upload

---
+++ wwp4.5.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+	float a, b, proizv, sum, chastnoe, raznost;
+	a != 0;
+	b != 0;
+	scanf_s("%f", &a);
+	scanf_s("%f", &b);
+	sum = a + b;
+	raznost = a - b;
+	proizv = a * b;
+	chastnoe = (a) / (b);
+	printf("a + b %f\n", sum);
+	printf("a - b %f\n", raznost);
+	printf("a * b %f\n", proizv);
+	printf("abs(a)/abs(b) %f", chastnoe);
+	return 0;
+}
