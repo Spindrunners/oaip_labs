739f16556e4d092f353085a479140f60cf3ea26a
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 17:48:01 +0300

    Add files via upload

---
+++ wwp17.3.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	int a[8];
+	int n;
+	printf("N=\n");
+	scanf_s("%i", &n);
+	int i;
+	for (i = 0; i < n; ++i)
+	{
+		printf("a[%i] : ", i + 1);
+		scanf_s("%i", &a[i]);
+	}
+	int min = a[1];
+	for (i = 1; i < n; i += 2)
+	{
+		if (a[i] < min) min = a[i];
+	}
+	printf("%i\n", min);
+	return 0;
+}
