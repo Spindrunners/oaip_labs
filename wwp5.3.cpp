fb7d2c07dffe7f76e8f08ec6414fd66eea65986c
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:07:55 +0300

    Add files via upload

---
+++ wwp5.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float x1, x2, x3, AC, BC, x;
+	printf("enter A: ");
+	scanf_s("%f", &x1);
+	printf("enter B: ");
+	scanf_s("%f", &x2);
+	printf("enter C: ");
+	scanf_s("%f", &x3);
+	AC = fabs(x3 - x1);
+	BC = fabs(x3 - x2);
+	x = AC * BC;
+	printf("AC * BC:%f", x);
+	return 0;
+}
