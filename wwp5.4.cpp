4ce76e05ee4f3a42b5efa60b6da982c9c4b5879c
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:10:11 +0300

    Add files via upload

---
+++ wwp5.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float x1, x2, y1, y2, a, b, p, s;
+	printf("enter x1: ");
+	scanf_s("%f", &x1);
+	printf("enter x2: ");
+	scanf_s("%f", &x2);
+	printf("enter y1: ");
+	scanf_s("%f", &y1);
+	printf("enter y2: ");
+	scanf_s("%f", &y2);
+	a = fabs(x2 - x1);
+	b = fabs(y2 - y1);
+	p = (a + b) * 2;
+	s = a * b;
+	printf("(a + b) * 2 = %f\n", p);
+	printf("a * b = %f", s);
+	return 0;
+}
