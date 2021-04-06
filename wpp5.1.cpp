be598bb47e1a2cd035c2780fa9d6161295f2182f
Spindrunners <73659968+Spindrunners@users.noreply.github.com>
2021-04-06 16:07:07 +0300

    Add files via upload

---
+++ wpp5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float x1, x2, y1, y2, z, x, c;
+	printf("enter x1, x2, y1, y2:\n");
+	scanf_s("%f", &x1);
+	scanf_s("%f", &x2);
+	scanf_s("%f", &y1);
+	scanf_s("%f", &y2);
+	z = (x2 - x1) * (x2 - x1);
+	x = (y2 - y1) * (y2 - y1);
+	c = sqrt(z + x);
+	printf("qrt(z + x) %f", c);
+	return 0;
+}
---
+++ wwp5.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float x1, x2, x3, ac, bc, s;
+	printf("enter a: ");
+	scanf_s("%f", &x1);
+	printf("enter b: ");
+	scanf_s("%f", &x2);
+	printf("enter c: ");
+	scanf_s("%f", &x3);
+	ac = fabs(x3 - x1);
+	bc = fabs(x3 - x2);
+	s = (ac + bc);
+	printf("ac=%f\n", ac);
+	printf("bc=%f\n", bc);
+	printf("(ac + bc)=%f", s);
+	return 0;
+}
